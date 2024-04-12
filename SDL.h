c8231226a28774913c16473c02006f6a26a0ed1d
Henk van Wersch <henkvanwersch123@gmail.com>
2024-04-12 23:59:05 +0200

    Add header file

---
+++ SDL.h
@@ -0,0 +1,7 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <stdbool.h>
+
+void renderText(SDL_Renderer* renderer, const char* text, int x, int y, int fontSize, bool centered);
+
+int main(int argc, char* argv[]);
