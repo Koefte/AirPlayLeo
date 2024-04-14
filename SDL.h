9d77a1ca88eba3ab8971bcc4fe6d2b2feb70ecdf
Koefte <119340015+Koefte@users.noreply.github.com>
2024-04-14 16:33:49 +0200

    SDL.h aktualisieren

--- SDL.h
+++ SDL.h
@@ -1,7 +1,10 @@
+
+
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 #include <stdbool.h>
 
 void renderText(SDL_Renderer* renderer, const char* text, int x, int y, int fontSize, bool centered);
-
-int main(int argc, char* argv[]);
+SDL_Window* initWindow(int width, int height);
+SDL_Renderer* initRenderer(SDL_Window* window);
+void clearScreen(SDL_Renderer* renderer, int r, int g, int b, int alpha);
